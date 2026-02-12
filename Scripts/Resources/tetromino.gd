75522f9b5bf87c8deb9dd67cd7ef9561ce50adf1
Nguyen Duc Tam <saitohikari197@gmail.com>
2026-02-12 15:27:22 +0700

    Defining data container for each type of tetromino

---
+++ Scripts/Resources/tetromino.gd
@@ -0,0 +1,20 @@
+extends Resource
+class_name Tetromino
+
+@export var piece_atlas: GameController.tetrominoes
+
+@export var rot0: Array[Vector2i]
+@export var rot90: Array[Vector2i]
+@export var rot180: Array[Vector2i]
+@export var rot270: Array[Vector2i]
+
+func get_piece_tiles(orientation: int) -> Array[Vector2i]:
+	match orientation:
+		1: 
+			return rot90
+		2:
+			return rot180
+		3:
+			return rot270
+		_: 
+			return rot0
